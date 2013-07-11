e111b76fe597330ea2cc692e83447828313b26c2
Mauricio Caro <maucaro30@hotmail.com>
2013-07-11 02:02:20 +0000

    Modified web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
+var buffer = new Buffer(fs.readFileSync("index.html"));
+var html = buffer.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
